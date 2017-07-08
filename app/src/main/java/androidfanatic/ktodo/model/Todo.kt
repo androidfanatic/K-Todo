b6489d5d0256c8de5c3fbc8dc9c5101ee7831165
Android Fanatic <manish_raj@outlook.com>
2017-07-08 22:34:28 +0530

    add widget

--- app/src/main/java/androidfanatic/ktodo/model/Todo.kt
+++ app/src/main/java/androidfanatic/ktodo/model/Todo.kt
@@ -5,7 +5,8 @@ import com.orm.SugarRecord
 // model
 data class Todo(
         var title: String = "",
-        var done: Boolean = false
+        var done: Boolean = false,
+        val timeAdded: Long = System.currentTimeMillis()
 ) : SugarRecord() {
     fun widgetText() = String.format(" %s %s\r\n", if (done) "\u2714" else "\u2718", title)
 }
